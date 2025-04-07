27e2aec72bc8bc235a412eb3a69dcea3623e888f
Fred Nora <frednora@outlook.com>
2025-04-06 21:53:09 -0300

    kernel: Low level reboot routine via kbd.

--- zcore/kernel/arch/x86_64/startup/header/header4.asm
+++ zcore/kernel/arch/x86_64/startup/header/header4.asm
@@ -16,8 +16,8 @@ _asm_initialize_module0:
 ; _asm_reboot:
 ;     Reboot the system via ps2 keyboard.
 ; Steps:
-; Wait for an empty Input Buffer.
-; Send the reboot call to the keyboard controller.
+; Wait for an empty Input Buffer with timeout, 
+; but still send reboot command even if buffer stays busy.
 ;
 
 PS2KEYBOARD_PORT        EQU  0x64
@@ -28,18 +28,35 @@ _asm_reboot:
 
     xor rax, rax
 
+; Timeout counter (arbitrary large value)
+    mov rcx, 100000
+
 ; Get value and test status
+; Testing for empty buffer with timeout.
+.LNotEmpty:
     in al, PS2KEYBOARD_PORT
     test al, 00000010b
-    jne _asm_reboot
+    je .LContinue ; Proceed if buffer becomes empty
+    loop .LNotEmpty
+
+; Continue sending the reboot command even if timeout occurs
+.LContinue:
+
+; (Write-Back and Invalidate Cache)
+; Copy cache back to the main memory and invalidate cache.
+; Forcing cache/RAM synchronization.
+    wbinvd
 
 ; Send command
     mov al, PS2KEYBOARD_CMD_REBOOT
     out PS2KEYBOARD_PORT, al
+
+; Infinite loop to halt
 .Lloop:
     cli
     hlt
     jmp .Lloop
+
 
 
 ;=====================================
