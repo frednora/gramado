26ccbd5238f9edd916204bc9e95540e3d358c0ac
Fred Nora <frednora@outlook.com>
2025-09-14 21:09:47 -0300

    cpp00: c++ in ring 3.

--- outside/cpp00/cpprt0.cpp
+++ outside/cpp00/cpprt0.cpp
@@ -1,5 +1,7 @@
-
-// This is a test.
+// cpprt0.cpp
+// This is the C++ runtime for Gramado OS.
+// It's gonna call the main() function for a c++ program.
+// This is a c++ ring 3 program for Gramado OS.
 
 extern "C" void cpprt0_main(unsigned long data)
 {
