d8a098d1e1a3de279406cab7babbf8499c95b789
Fred Nora <frednora@outlook.com>
2025-10-17 10:14:34 -0300

    Gramado OS: Version 0.8 Build 352

--- kernel/include/config/version.h
+++ kernel/include/config/version.h
@@ -10,7 +10,7 @@
 
 // Current build number
 // Updated by the builder
-#define __CURRENT_BUILD_NUMBER    351
+#define __CURRENT_BUILD_NUMBER    352
 
 // Version
 #define VERSION_MAJOR    0
@@ -21,7 +21,7 @@
 #define VERSION_WORD    0x0008
 // Strings
 #define VERSION_STRING    "0.8"
-#define BUILD_STRING      "351"
+#define BUILD_STRING      "352"
 #define BUILD_CODENAME_STRING  "MACRO HARD"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
