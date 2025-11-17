39010e03f2aa649bd1f11191787929cff1fc7447
Fred Nora <frednora@outlook.com>
2025-11-17 10:09:37 -0300

    Gramado OS Build 354

--- kernel/include/config/version.h
+++ kernel/include/config/version.h
@@ -10,7 +10,7 @@
 
 // Current build number
 // Updated by the builder
-#define __CURRENT_BUILD_NUMBER    353
+#define __CURRENT_BUILD_NUMBER    354
 
 // Version
 #define VERSION_MAJOR    0
@@ -21,8 +21,8 @@
 #define VERSION_WORD    0x0008
 // Strings
 #define VERSION_STRING    "0.8"
-#define BUILD_STRING      "353"
-#define BUILD_CODENAME_STRING  "Otter"
+#define BUILD_STRING      "354"
+#define BUILD_CODENAME_STRING  "Giant Otter"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
 #endif  
