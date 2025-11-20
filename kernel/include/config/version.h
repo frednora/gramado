b56cfd1e464c8dc938b836536d21d6dfa6356d3d
Fred Nora <frednora@outlook.com>
2025-11-19 22:34:57 -0300

    Gramado OS Build 357

--- kernel/include/config/version.h
+++ kernel/include/config/version.h
@@ -10,7 +10,7 @@
 
 // Current build number
 // Updated by the builder
-#define __CURRENT_BUILD_NUMBER    356
+#define __CURRENT_BUILD_NUMBER    357
 
 // Version
 #define VERSION_MAJOR    0
@@ -21,8 +21,8 @@
 #define VERSION_WORD    0x0008
 // Strings
 #define VERSION_STRING    "0.8"
-#define BUILD_STRING      "356"
-#define BUILD_CODENAME_STRING  "Panda 00"
+#define BUILD_STRING      "357"
+#define BUILD_CODENAME_STRING  "Panda"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
 #endif  
