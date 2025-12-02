fae1c5a628129dcea2cb8b40045381d241fe0b05
Fred Nora <frednora@outlook.com>
2025-12-02 11:16:37 -0300

    Gramado OS: Build 359

--- kernel/include/config/version.h
+++ kernel/include/config/version.h
@@ -10,7 +10,7 @@
 
 // Current build number
 // Updated by the builder
-#define __CURRENT_BUILD_NUMBER    358
+#define __CURRENT_BUILD_NUMBER    359
 
 // Version
 #define VERSION_MAJOR    0
@@ -21,8 +21,8 @@
 #define VERSION_WORD    0x0008
 // Strings
 #define VERSION_STRING    "0.8"
-#define BUILD_STRING      "358"
-#define BUILD_CODENAME_STRING  "Current Display"
+#define BUILD_STRING      "359"
+#define BUILD_CODENAME_STRING  "Callback"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
 #endif  
