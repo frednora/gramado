db267febfb99b747b96407a21dc61807164c0c96
Fred Nora <frednora@outlook.com>
2026-06-09 22:14:41 -0300

    cmds: Create the project lua1/

---
+++ np/cmds/lua1/main.c
@@ -0,0 +1,25 @@
+// main.c
+// Lua 1.0 for Gramado OS
+
+
+// rtl
+#include <types.h>
+#include <ctype.h>
+#include <heap.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+
+    // #todo
+    // See: lua.c
+    // lua_main(argc, argv);
+
+    return 0;
+}
+
